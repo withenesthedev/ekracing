da16601afbcf8593fc93be5de1060bcca59db77a
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2023-12-10 19:56:11 +0300

    initial astro app with tailwind, compress, seo  and icon integrations

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [compress({ Logger: false }), tailwind()],
+});
---
+++ prettier.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Config} */
+export default {
+  useTabs: true,
+  singleQuote: true,
+  semi: false,
+  printWidth: 100,
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+};
