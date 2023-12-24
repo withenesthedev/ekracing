8126ea5cc2d5bcab599a9080970fd6934afe02e7
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2023-12-24 16:07:49 +0300

    refactor remove font support tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,6 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ['Helvetica Neue', 'Helvetica', 'Arial', ...defaultTheme.fontFamily.sans],
-			},
-		},
-	},
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,json}'],
+	theme: {},
 	plugins: [],
 };
