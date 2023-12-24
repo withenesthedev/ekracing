02a998a474b81a6a3607be059203c68a52f1fa07
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2023-12-24 16:43:11 +0300

    add og image support
    refactor change content type to data

--- src/collections/ServiceCollection.ts
+++ src/collections/ServiceCollection.ts
@@ -1,7 +1,7 @@
 import { defineCollection, z } from "astro:content";
 
 export const ServiceCollection = defineCollection({
-    type: 'content',
+    type: 'data',
     schema: z.object({
       title: z.string(),
       thumbnail: z.string(),
